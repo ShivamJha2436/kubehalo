0160a65587718feb1721ee0902d804599cd4672d
Shivam Kumar <shivamkumar87148@gmail.com>
2025-08-04 10:00:02 +0530

    Added handling informer and cache logic
    Signed-off-by: Shivam Kumar <shivamkumar87148@gmail.com>

--- controllers/scalepolicy/lister.go
+++ controllers/scalepolicy/lister.go
@@ -1,1 +1,34 @@
 package scalepolicy
+
+import (
+	"log"
+	"sync"
+
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/cache"
+
+	v1 "kubehalo/api/v1"
+)
+
+var (
+	scalePolicyCache = make(map[string]v1.ScalePolicy)
+	mu               sync.RWMutex
+)
+
+func StartScalePolicyInformer(clientset *kubernetes.Clientset, stopCh <-chan struct{}) {
+	// TODO: Use dynamic client / generated CRD client
+	log.Println("[INFORMER] Starting ScalePolicy informer...")
+
+	// Dummy: In real implementation, this will watch CRD changes
+}
+
+func GetCurrentScalePolicies() []v1.ScalePolicy {
+	mu.RLock()
+	defer mu.RUnlock()
+
+	var list []v1.ScalePolicy
+	for _, p := range scalePolicyCache {
+		list = append(list, p)
+	}
+	return list
+}
