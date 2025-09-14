277b95fff408bb270efc49883b18a3f05328bc27
Shivam Kumar <shivamkumar87148@gmail.com>
2025-09-14 17:56:02 +0530

    Add BehaviorSpec support to ScalePolicy handler and scaling engine
    Signed-off-by: Shivam Kumar <shivamkumar87148@gmail.com>

--- internal/scaling/engine.go
+++ internal/scaling/engine.go
@@ -3,26 +3,30 @@ package scaling
 import (
 	"context"
 	"fmt"
+
 	"k8s.io/client-go/kubernetes"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
+// ScalingEngine handles scaling operations for Deployments
 type ScalingEngine struct {
-	clientset *kubernetes.Clientset
+	client kubernetes.Interface
 }
 
-func NewScalingEngine(clientset *kubernetes.Clientset) *ScalingEngine {
-	return &ScalingEngine{clientset: clientset}
+// NewScalingEngine accepts kubernetes.Interface instead of *Clientset
+func NewScalingEngine(client kubernetes.Interface) *ScalingEngine {
+	return &ScalingEngine{client: client}
 }
 
+// ScaleDeployment scales a Deployment to the desired number of replicas
 func (s *ScalingEngine) ScaleDeployment(namespace, name string, replicas int32) error {
-	deploy, err := s.clientset.AppsV1().Deployments(namespace).Get(context.TODO(), name, metav1.GetOptions{})
+	deploy, err := s.client.AppsV1().Deployments(namespace).Get(context.TODO(), name, metav1.GetOptions{})
 	if err != nil {
 		return err
 	}
 
 	deploy.Spec.Replicas = &replicas
-	_, err = s.clientset.AppsV1().Deployments(namespace).Update(context.TODO(), deploy, metav1.UpdateOptions{})
+	_, err = s.client.AppsV1().Deployments(namespace).Update(context.TODO(), deploy, metav1.UpdateOptions{})
 	if err != nil {
 		return err
 	}
